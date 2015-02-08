d9a54f2d231f883052fe800e31eec94c05fc2e7d
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-02-08 17:22:25 +0100

    Very simple .NET Console

---
+++ NETGalileo/Helpers/PrimeHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace NETGalileo.Helpers
+{
+    public static class PrimeHelper
+    {
+        public static bool IsPrime(int number)
+        {
+            if ((number & 1) == 0)
+                return number == 2;
+
+            for (int i = 3; (i * i) <= number; i += 2)
+            {
+                if ((number % i) == 0)
+                    return false;
+            }
+
+            return number != 1;
+        }
+    }
+}
---
+++ NETGalileo/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace NETGalileo
+{
+    using System;
+    using Helpers;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Primes between 0 and 100 ---");
+
+            for (int i = 0; i < 100; i++)
+            {
+                bool prime = PrimeHelper.IsPrime(i);
+                if (prime)
+                {
+                    Console.Write("Prime: ");
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
